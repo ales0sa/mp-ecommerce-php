036d478b1320b99b89efc9236f08c9f2c24adf75
Alejandro <alesosa@gmail.com>
2020-10-05 00:01:09 -0300

    X

--- success.php
+++ success.php
@@ -68,7 +68,7 @@
                 echo "<p>collection_status: ".$_GET["collection_status"]."</p>";
                 echo "<p>external_reference: ".$_GET["external_reference"]."</p>";
                 echo "<p>payment_type: ".$_GET["payment_type"]."</p>";
-                echo "<p>merchant_order_id: ".$_GET["merchant_order_id"]."</p>";
+                //echo "<p>merchant_order_id: ".$_GET["merchant_order_id"]."</p>";
                 echo "<p>preference_id: ".$_GET["preference_id"]."</p>";
                 echo "<p>site_id: ".$_GET["site_id"]."</p>";
                 echo "<p>processing_mode: ".$_GET["processing_mode"]."</p>";
